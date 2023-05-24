ac2d9fbe4800eff7475476ae8c3a937fc78abaf0
tomclaine <leduykhanh219@gmail.com>
2023-05-24 16:48:07 +0700

    product-detail
    amount quantity product

--- assets/js/productDetail.js
+++ assets/js/productDetail.js
@@ -18,6 +18,34 @@ img.addEventListener('mouseleave',()=> {
     result.classList.add('hide');
 })
 
+let amountElement = document.getElementById("amount");
+let amount = amountElement.value;
+console.log(amount);
+let btnReduce = document.querySelector('.reduce-btn');
+let btnIncrease = document.querySelector('.increase-btn');
+let render = (amount)=>{
+    amountElement.value = amount;
+}
+btnIncrease.addEventListener('click',()=>{
+    amount++;
+    render(amount);
+})
+
+btnReduce.addEventListener('click',()=>{
+    if(amount>1){
+        amount--;
+        render(amount);
+    }
+})
+amountElement.addEventListener('input',()=>{
+    amount = amountElement.value;
+    amount = parseInt(amount);
+    amount = (isNaN(amount)||amount==0)?1:amount;
+    render(amount);
+})
+
+
+
 let alertCart = document.querySelector(".add-cart");
 alertCart.addEventListener("click",function(e){
     alert("Bạn đã thêm sản phẩm vào giỏ hàng thành công!");
